7eb19bf4c0b58067dba90ad431ed73a002a5f906
Hanna Böös <ens21hbs@cs.umu.se>
2024-02-13 11:06:10 +0100

    lagt till knapp för task 1

---
+++ script.js
@@ -0,0 +1,7 @@
+/styling för task 1/ 
+function onButtonClick() {
+	alert('Hello, JavaScript!');
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', onButtonClick);
